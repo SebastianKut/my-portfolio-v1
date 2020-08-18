c185ce206ad2bfe98c3764760578ffdb0a42162e
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-08-18 15:15:09 +0200

    added copy to clipboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,3 +184,43 @@
   });
 
 })(jQuery);
+
+//add smooth scrolling to get in touch button 
+document.getElementById('get-in-touch').addEventListener('click', function(e) {
+                // Prevent scrolling if the 
+                // hash value is blank 
+                e.preventDefault(); 
+          
+                // Get the destination to scroll to 
+                // using the hash property 
+                let destination =  
+                    document.querySelector(this.hash); 
+          
+                // Scroll to the destination using 
+                // scrollIntoView method 
+                destination.scrollIntoView({ 
+                    behavior: 'smooth' 
+                }); 
+
+
+});
+
+//copy email to clipboard
+
+document.getElementById('email-text').addEventListener('click', (e) =>{
+e.preventDefault();
+
+navigator.clipboard.writeText(e.target.textContent).then(function() {
+    // Promise resolved successfully.
+    console.log("Copied to clipboard successfully!");
+    let confirmationMessage = document.getElementById('email-copied-confirm');
+    confirmationMessage.style.display = 'block';
+    setTimeout(function(){confirmationMessage.style.display = 'none';}, 3000);
+  }, function() {
+    // Promise rejected.
+    console.error("Unable to write to clipboard. :-(");
+  });
+
+});
+
+
